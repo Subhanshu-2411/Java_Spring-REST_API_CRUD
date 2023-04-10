d22e9d59e69938a8347a814f58a4153900b4d9e1
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-10 11:34:38 +0530

    Added Hello World Application

--- src/main/java/com/example/java_springrest_api_crud/JavaSpringRestApiCrudApplication.java
+++ src/main/java/com/example/java_springrest_api_crud/JavaSpringRestApiCrudApplication.java
@@ -2,12 +2,20 @@ package com.example.java_springrest_api_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaSpringRestApiCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringRestApiCrudApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
     }
 
 }
