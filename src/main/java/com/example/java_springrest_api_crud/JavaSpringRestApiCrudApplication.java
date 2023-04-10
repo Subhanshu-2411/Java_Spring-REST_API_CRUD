eedde630b90ca078e944843e6f67aa2c883e0fa8
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-10 12:03:06 +0530

    Adding Service and Business Layer

--- src/main/java/com/example/java_springrest_api_crud/JavaSpringRestApiCrudApplication.java
+++ src/main/java/com/example/java_springrest_api_crud/JavaSpringRestApiCrudApplication.java
@@ -1,17 +1,9 @@
 package com.example.java_springrest_api_crud;
 
-import com.example.java_springrest_api_crud.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class JavaSpringRestApiCrudApplication {
 
     public static void main(String[] args) {
@@ -22,18 +14,5 @@ public class JavaSpringRestApiCrudApplication {
 //    public String hello() {
 //        return "Hello World";
 //    }
-
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Subhanshu Bansal",
-                        "subhanshu.bansal5566@gmail.com",
-                        LocalDate.of(2001, Month.NOVEMBER, 24),
-                        21
-                )
-        );
-    }
 
 }
