8480549ebec3c1ecbd6efe490a5909f1cd8502c3
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-10 13:08:41 +0530

    Added Student Repository

--- src/main/java/com/example/java_springrest_api_crud/student/StudentService.java
+++ src/main/java/com/example/java_springrest_api_crud/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.java_springrest_api_crud.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    
+    private final StudentRepository studentRepository;
+    
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Subhanshu Bansal",
-                        "subhanshu.bansal5566@gmail.com",
-                        LocalDate.of(2001, Month.NOVEMBER, 24),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
